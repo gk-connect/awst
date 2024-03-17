5256c453151ebcf088a21dbdf8f911f934dea098
Vogo <vogo@gk.local>
2024-03-17 23:32:48 +0530

    connect command added

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
-Usage
+Here's the markdown code for the GitHub README.md file:
 
+```markdown
+# AWST (Amazon Web Services Tool)
+
+AWST is a command-line tool for managing AWS resources. It provides various commands to interact with AWS services.
+
+## Installation
+
+To install AWST, run:
+```
+```bash
+sudo cp awst /usr/local/bin/awst
+```
+
+## Usage
+
+### List EC2 Instances
+
+To list EC2 instances, use the following command:
+
+```bash
 awst list ec2 --profile <aws_config_profile> --filter <filter-text>
+```
+
+### Connect to an EC2 Instance
+
+To connect to an EC2 instance, use the following command:
+
+```bash
+awst connect --profile <aws_config_profile> --filter <filter-text>
+```
+
+#### Optional Args:
+
+- `--region`: Specify the AWS region. Default is `ap-south-1`.
+- `--ssh-ip-type`: Specify the type of IP to use for SSH. Default is `private`.
+- `--key`: Specify the keypair to use for SSH authentication. Only required if the instance doesn't have a keypair attached during launch or you want to use a different key. Default is the key pair provided during instance launch.
+- `--key-path`: Specify the folder where your SSH key resides. Default is `~/.ssh/`.
+
+## Examples
+
+List all EC2 instances:
+
+```bash
+awst list ec2 --profile myprofile --filter *
+```
+
+Connect to an EC2 instance using a specific key:
+
+```bash
+awst connect --profile myprofile --filter myinstance --key mykey --key-path /path/to/key
+```
+
+## Contributing
+
+Contributions are welcome! Please fork this repository and open a pull request with your changes.
+
+
--- cmd/connect.go
+++ cmd/connect.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -12,15 +13,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var connectIp string
+var connectKey string
+var connectKeyPath string
+
 // connectCmd represents the connect command
 var connectCmd = &cobra.Command{
 	Use:   "connect",
 	Short: "A brief description of your command",
 	Long:  "connect to aws ec2 isntances",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("connect called")
-		fmt.Println(profile)
+		if connectIp != "private" && connectIp != "public" {
+			errorColor.Println("Available SSH Ip options are private/public")
+			return
+		}
+
+		if !(len(filter) > 0) {
+			errorColor.Println("Use --filter flag with valid data")
+			return
+		}
 		getEc2Instances()
+
 	},
 }
 
@@ -30,6 +43,9 @@ func init() {
 	connectCmd.Flags().StringVarP(&profile, "profile", "p", "", "AWS profile")
 	connectCmd.Flags().StringVarP(&region, "region", "r", defaultRegion, "AWS region")
 	connectCmd.Flags().StringVarP(&filter, "filter", "f", "", "Filter")
+	connectCmd.Flags().StringVarP(&connectIp, "ssh-ip-type", "i", "private", "SSH Ip Adress")
+	connectCmd.Flags().StringVarP(&connectKey, "key", "k", "NONE", "SSH Key")
+	connectCmd.Flags().StringVarP(&connectKeyPath, "key-path", "l", "~/.ssh/", "SSH Key Path")
 }
 
 func getEc2Instances() {
@@ -48,8 +64,12 @@ func getEc2Instances() {
 	input := &ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
 			{
-				Name:   aws.String("tag:Name"),
-				Values: []*string{aws.String("*JENKINS*")},
+				Name: aws.String("tag:Name"),
+				Values: []*string{
+					aws.String("*" + strings.ToUpper(filter) + "*"),
+					aws.String("*" + strings.ToLower(filter) + "*"),
+					aws.String("*" + strings.ToTitle(filter) + "*"),
+				},
 			},
 		},
 	}
@@ -59,44 +79,98 @@ func getEc2Instances() {
 		return
 	}
 
+	// fmt.Println(result)
+
 	ec2List := []string{}
+	ec2SshKey := []string{}
+	ec2Ip := []string{}
+	ec2Data := make(map[string][]string)
 	for _, reservation := range result.Reservations {
 		for _, instance := range reservation.Instances {
-			fmt.Println("Instance ID:", *instance.InstanceId)
-			fmt.Println("Instance State:", *instance.State.Name)
+			instanceName := "-"
+			// fmt.Println(*instance.InstanceId)
+			if len(instance.Tags) > 0 {
+				for _, tag := range instance.Tags {
+					if *tag.Key == "Name" {
+						instanceName = *tag.Value
+						break
+					}
+				}
+			}
+			publicIp := "-"
+			if instance.PublicIpAddress != nil {
+				publicIp = *instance.PublicIpAddress
+			}
+			privateIp := "-"
+			if instance.PrivateIpAddress != nil {
+				privateIp = *instance.PrivateIpAddress
+			}
 
-			ec2List = append(ec2List, *instance.InstanceId)
-			// Add more details if needed
+			if connectIp == "public" {
+				ec2Ip = append(ec2Ip, publicIp)
+			} else {
+				ec2Ip = append(ec2Ip, privateIp)
+			}
+			if instance.KeyName != nil {
+				ec2SshKey = append(ec2SshKey, *instance.KeyName)
+			} else {
+				ec2SshKey = append(ec2SshKey, connectKey)
+			}
+
+			ec2List = append(ec2List, instanceName+" ["+*instance.InstanceId+"]  => "+publicIp+" / "+privateIp)
+
 		}
 	}
+	ec2Data["ec2_list"] = ec2List
+	ec2Data["ec2_ip"] = ec2Ip
+	ec2Data["ec2_key"] = ec2SshKey
 
-	interactivePrompt(ec2List)
+	interactivePrompt(ec2Data)
 
 }
 
-func interactivePrompt(ec2List []string) {
-	prompt := promptui.Select{
-		Label: "Select your favorite programming language",
-		Items: ec2List,
+func interactivePrompt(ec2Data map[string][]string) {
+	ec2_prompt := promptui.Select{
+		Label: "Select your instance to SSH",
+		Items: ec2Data["ec2_list"],
 	}
 
-	_, result, err := prompt.Run()
+	_, ec2Item, err := ec2_prompt.Run()
 	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-
+		errorColor.Printf("Prompt failed %v\n", err)
 		return
 	}
 
-	fmt.Printf("You selected: %s\n", result)
-	initateSshSession("PROD.pem", "ubuntu", "110.130.345.221")
+	selectItemIndex := findIndex(ec2Data["ec2_list"], ec2Item)
+
+	user_prompt := promptui.Select{
+		Label: "Select your instance to SSH",
+		Items: []string{"ubuntu", "root", "ec2-user", "admin", "centos", "debian"},
+	}
+
+	_, ec2User, err := user_prompt.Run()
+	if err != nil {
+		errorColor.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	initateSshSession(ec2Data["ec2_key"][selectItemIndex]+".pem", ec2User, ec2Data["ec2_ip"][selectItemIndex])
 
 }
 
 func initateSshSession(sshKey string, sshUser string, sshIp string) {
+
+	if sshKey == "NONE.pem" {
+		fmt.Println("No Valid Key Found")
+		return
+	}
+
+	successColor.Println("Executing.. ssh -i " + connectKeyPath + sshKey + " " + sshUser + "@" + sshIp)
+
 	cmd := "ssh"
 
 	// Arguments for the command
-	args := []string{"-i", "~/.ssh/" + sshKey, sshUser + "@" + sshIp}
+	args := []string{"-i", connectKeyPath + sshKey, sshUser + "@" + sshIp}
 
 	// Execute the command
 	session := exec.Command(cmd, args...)
@@ -106,7 +180,15 @@ func initateSshSession(sshKey string, sshUser string, sshIp string) {
 
 	err := session.Run()
 	if err != nil {
-		fmt.Println("Error:", err)
+		errorColor.Println("Error:", err)
 		return
 	}
+}
+func findIndex(slice []string, item string) int {
+	for i, v := range slice {
+		if v == item {
+			return i
+		}
+	}
+	return -1 // Item not found
 }
